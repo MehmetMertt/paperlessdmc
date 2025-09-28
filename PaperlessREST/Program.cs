cfd810ff83d0462539a737cf3072c0a5ae5895ee
mklycs <michael.klys@gmail.com>
2025-09-28 21:49:26 +0100

    explenation

--- PaperlessREST/Program.cs
+++ PaperlessREST/Program.cs
@@ -4,39 +4,35 @@ using PaperlessREST.Infrastructure;
 using PaperlessREST.Infrastructure.Repositories;
 using PaperlessREST.Infrastructure.Service;
 
-var builder = WebApplication.CreateBuilder(args);
+var builder = WebApplication.CreateBuilder(args); // an object which stores configurations (env-variables, etc.) to later build the webapp
 
-
-builder.Services.AddDbContext<PaperlessRestContext>(options =>
-    options.UseNpgsql(
-        builder.Configuration.GetConnectionString("Default"),
-        b => b.MigrationsAssembly("PaperlessREST.Infrastructure")
+builder.Services.AddDbContext<PaperlessRestContext>(options =>    // specifies context (connection to database, manages database entities, follows changes on objects) 
+    options.UseNpgsql(                                            // usenpgsql ... uses postgres as database
+        builder.Configuration.GetConnectionString("Default"),     // "Default" ... uses the connection specified in appsettings.json
+        b => b.MigrationsAssembly("PaperlessREST.Infrastructure") // MigrationsAssembly ... specifies where ef-migrations are saved
     )
 );
 
+// scope ... lifetime of the service per HTTP request
+builder.Services.AddScoped<IMetaDataService, MetaDataService>();       // MetaDataService ... consists of functions for metadata CRUD operations
+builder.Services.AddScoped<IMetadataRepository, MetadataRepository>(); // Repository pattern for data access
 
-builder.Services.AddScoped<IMetaDataService, MetaDataService>();
-
-
-builder.Services.AddScoped<IMetadataRepository, MetadataRepository>();
-// Add services to the container.
-
-builder.Services.AddControllers();
+builder.Services.AddControllers(); // registers MVC-controller, without this aps.net wouldnt know which classes to use for HTTP request (endpoint /api/metadata)
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-using (var scope = app.Services.CreateScope())
+// initializes the database here
+using (var scope = app.Services.CreateScope()) // scope ... small container for services
 {
     var services = scope.ServiceProvider;
     try
     {
         var context = services.GetRequiredService<PaperlessRestContext>();
-
-        context.Database.EnsureDeleted();
-        context.Database.EnsureCreated();
+        context.Database.EnsureDeleted(); // deletes database on start (only good for development)
+        context.Database.EnsureCreated(); // creates database on start
     }
     catch (Exception ex)
     {
@@ -45,19 +41,12 @@ using (var scope = app.Services.CreateScope())
     }
 }
 
-
-
-// Configure the HTTP request pipeline.
 /*if (app.Environment.IsDevelopment())
 {*/
 app.UseSwagger();
 app.UseSwaggerUI();
 /*}*/
-
-app.UseHttpsRedirection();
-
+app.UseHttpsRedirection(); // redirects HTTP to HTTPS
 app.UseAuthorization();
-
-app.MapControllers();
-
+app.MapControllers();      // connects controller endpoints through routing
 app.Run();
--- PaperlessRest.Application/Commands/CreateMetaDataCommand.cs
+++ PaperlessRest.Application/Commands/CreateMetaDataCommand.cs
@@ -3,11 +3,11 @@ using System.Diagnostics.CodeAnalysis;
 
 namespace PaperlessREST.Infrastructure.Service
 {
-    public record CreateMetaDataCommand(
+    public record CreateMetaDataCommand( // specifies the format of the query matching to the table
         [StringLength(100, MinimumLength = 3, ErrorMessage = "Name must be between 3 and 100 characters")]
         string Name,
         Guid OwnerId,
-        [StringLength(20,MinimumLength =2,ErrorMessage ="File exntension must be between 2 and 20 characters")]
+        [StringLength(20,MinimumLength = 2,ErrorMessage ="File exntension must be between 2 and 20 characters")]
         string FileExtension,
         string? Author
     );
